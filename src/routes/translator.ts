6db9f66076b4ce5cbbc03ca39c3f0a78502f5be6
academy_intern <academy_intern@academy-interns-MacBook-Pro.local>
2018-10-07 11:53:34 +0200

    Added json object listener and ready for http post requests

--- src/routes/translator.ts
+++ src/routes/translator.ts
@@ -1,23 +1,42 @@
 import {Request, Response} from "express";
 
-import ussd = require('../db.json'); 
+//A local database
+let ussd = require('../db.json'); 
 
-//Get a parser for converting JSON Objects to XM
-import parser = require('json-xml-parser');
+//A module to convert JSON object to XML file
+let js2xmlparser = require("js2xmlparser");
 
 export class Translator { 
     
     public routes(app): void {        
        
       
-        app.route('/getdata/:id')
-        .get((req:Request, res: Response) => {
-           let id = req.params.id;
-           var userbody = req.body;
+        app.route('/ussddata')
+        .post((req:Request, res: Response) => {
 
-           var xml = parser.jsonToXml(ussd);
-           console.log(xml);
-           res.status(200).send(userbody);
+            //convert JSON to XML from the user request
+            let xmlDataClientReq  = js2xmlparser.parse("USSD", req.body);
+
+            //convert JSON to XML from the local database
+            let xmlDataLocalDatabase = js2xmlparser.parse("USSD", ussd);
+
+            //Print results of the local DB on the console
+            console.log(xmlDataLocalDatabase);
+            
+            //Send requested results to the client
+            res.status(200).send(xmlDataClientReq);
+
+            res.end();
+
+        })
+
+        app.route('/getdata')
+        .get((req:Request, res: Response) => {
+
+            
+            res.status(200).send(ussd);
+
+            res.end();
 
         })
     }
